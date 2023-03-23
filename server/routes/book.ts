70c7b73463a6f180a4ce1740d971a3ba1f42aa06
Jean Lin <jeanlin0147@gmail.com>
2023-03-23 22:44:40 +0800

    feat: multi-keywords searching

--- server/routes/book.ts
+++ server/routes/book.ts
@@ -1,25 +1,37 @@
 import express from "express";
 import { ObjectId, Filter } from "mongodb";
 import { getDb } from "../db/conn";
-import { Book } from "../model/book";
-import { Search } from "../model/search";
 import { isArrayOfString } from "../helper";
+import { Book, BookRequest } from "../model/book";
+import { TypedRequestQuery } from "../model/request";
+import { Search } from "../model/search";
 
 export const bookRoutes = express.Router();
 
-bookRoutes.route("/book").get(function (req: express.Request<Search>, res) {
-  let query = {} as Filter<Book>;
+bookRoutes.route("/book").get(function (req: TypedRequestQuery<Search>, res) {
+  let query: Filter<Book> = {};
   if (req.query.author) {
-    query.authors = { $elemMatch: { $regex: `.*${req.query.author}.*` } };
+    query.authors = { $elemMatch: { $regex: `.*${req.query.author}.*`, $options: 'i' } };
   }
-  if (req.query.keyword) {
-    query.$or = [
-      { title: { $regex: `.*${req.query.keyword}.*` } },
-      { description: { $regex: `.*${req.query.keyword}.*` } }
-    ];
+  if (Array.isArray(req.query.keywords)) {
+    req.query.keywords.forEach(keyword => {
+      query.$and = [
+        ...query.$and ? query.$and : [],
+        {
+          $or: [
+            { title: { $regex: `.*${keyword}.*`, $options: 'i' } },
+            { description: { $regex: `.*${keyword}.*`, $options: 'i' } }
+          ]
+        }
+      ];
+    })
+  } else {
+    return res.status(400).json({ result: false });
   }
   if (Array.isArray(req.query.labels)) {
     query.labels = { $all: req.query.labels };
+  } else {
+    return res.status(400).json({ result: false });
   }
   console.debug("query:", JSON.stringify(query));
   getDb().collection<Book>("book").find(query).toArray().then((books) => {
@@ -31,7 +43,7 @@ bookRoutes.route("/book").get(function (req: express.Request<Search>, res) {
 });
 
 bookRoutes.route("/book/:bookId").get(function (req, res) {
-  let query = { _id: new ObjectId(req.params.bookId) };
+  let query: Filter<Book> = { _id: new ObjectId(req.params.bookId) };
   getDb().collection<Book>("book").findOne(query).then(function (book) {
     return res.json({ result: true, book: book });
   }).catch(error => {
@@ -40,14 +52,14 @@ bookRoutes.route("/book/:bookId").get(function (req, res) {
   });;
 });
 
-bookRoutes.route("/book").post(function (req, res) {
+bookRoutes.route("/book").post(function (req: express.Request<BookRequest>, res) {
   try {
     if (
-      isArrayOfString(req.body.book.authors)
-      && typeof req.body.book.coverImage === 'string'
-      && typeof req.body.book.description === 'string'
-      && isArrayOfString(req.body.book.labels)
-      && typeof req.body.book.title === 'string'
+      isArrayOfString(req.body.authors)
+      && typeof req.body.coverImage === 'string'
+      && typeof req.body.description === 'string'
+      && isArrayOfString(req.body.labels)
+      && typeof req.body.title === 'string'
     ) {
       const newBook: Book = {
         authors: req.body.book.authors,
@@ -72,4 +84,14 @@ bookRoutes.route("/book").post(function (req, res) {
     console.error(error);
     return res.status(500).json({ result: false });
   }
+});
+
+bookRoutes.route("/book/:bookId").delete(function (req, res) {
+  let query: Filter<Book> = { _id: new ObjectId(req.params.bookId) };
+  getDb().collection<Book>("book").deleteOne(query).then(function () {
+    return res.status(204);
+  }).catch(error => {
+    console.error(error);
+    return res.status(500).json({ result: false });
+  });;
 });
